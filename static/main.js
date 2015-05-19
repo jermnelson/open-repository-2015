64fe492fe8160d66a55bdf6a7570d449c99bfa89
Jeremy Nelson <jermnelson.gmail.com>
2015-05-19 16:55:55 -0600

    continued to debug angular

--- static/main.js
+++ static/main.js
@@ -1,8 +1,8 @@
 var slides = [
  { title: 'Obligatory S.H.E.I.L.D. Reference'},
- { title: 'Slide 2' },
- { title: 'Slide 3' },
- { title: 'Slide 4' },
+ { title: 'Language, Frameworks, Meh!' },
+ { title: 'Connecting People and Institutions with Algorithms' },
+ { title: 'Slide 4 ' },
  { title: 'Slide 5' },
  { title: 'Slide 6' },
  { title: 'Slide 7' },
@@ -30,9 +30,25 @@ var slides = [
   'use strict';
   
   angular.module('or2015App', [])
-   .controller('OR2015AppController', function() {
-     var or2015 = this;
+   .controller('OR2015AppController', ['$scope', function($scope) {
+     $scope.currentSlide = 1;
 
+/*
+     $scope.slideCount = slides.length;
+     $scope.slideTitle = slides[$scope.currentSlide-1]['title'];
+     $scope.countdown = 420;
+*/
+     $scope.goSlide = function() {
+       console.log("In goSlide");
+
+     };
+
+     $scope.advanceSlide = function() {
+      $scope.currentSlide += 1;
+      $scope.slideTitle = slides[$scope.currentSlide-1]['title'];
+      console.log("In advancedSlide " + $scope.currentSlide + "\n" + $scope.slideTitle);
+     };
+/*
      for(var i; i<=slides.length; i++) {     
         var slide = slides[i];
         var countdown = 420-(i*17.5);
@@ -44,7 +60,8 @@ var slides = [
                      or2015.countdown = 420;
                    }, 17000*i);
      };
-   })
+*/
+   }])
 
 
 }());
